85dbd48b913850ef35d6579508e76482ec017efb
Charllys de Lima <charllys.lima@outlook.com>
2022-07-07 22:53:22 -0300

    adjust arrangement of buttons and effects

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const uriApi = "https://api.qrserver.com/v1/create-qr-code/";
 
 /* elements */
 let buttonGenerate = document.getElementById("generate");
+let buttonDownload = document.getElementById("download");
+
 let imgQrCode = document.getElementById("qr");
 let inputText = document.getElementById("text");
-let spanError = document.getElementById("error");
+let msgError = document.getElementById("error");
+
 
 buttonGenerate.addEventListener("click", (e) => {
     clear();
     let text = document.getElementById("text").value;
     if (text) {
         generateQrCode(text, "500x500");
-        spanError.classList.add("d-none");
+        msgError.classList.add("d-none");
         imgQrCode.classList.remove("d-none");
+        buttonDownload.classList.remove("d-none");
     } else {
-        spanError.innerHTML = "Text is not a valid";
-        spanError.classList.remove("d-none");
+        msgError.innerHTML = "Text is not a valid";
+        msgError.classList.remove("d-none");
     }
 });
 
@@ -25,9 +29,10 @@ function generateQrCode(text, size) {
 }
 
 function clear() {
-    spanError.innerHTML = "";
+    msgError.innerHTML = "";
     imgQrCode.setAttribute("src", "");
 
     imgQrCode.classList.add("d-none");
-    spanError.classList.add("d-none");
+    msgError.classList.add("d-none");
+    buttonDownload.classList.add("d-none");
 }
